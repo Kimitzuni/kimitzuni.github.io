15b674ae351a2fa0323152c467a4c828d2e055d4
Rebecca White <rwhitego@gmail.com>
2022-07-27 17:21:46 +0100

    AAAAAAAAAAAAAAAAAAAAa

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function showProjects() {
+    $("#projects").fadeIn();
+    $("#contact").fadeOut();
+}
+
+function hideProjects() {
+    $("#projects").fadeOut();
+}
+
+function showContact() {
+    $("#contact").fadeIn();
+    $("#projects").fadeOut();
+}
+
+function hideContact() {
+    $("#contact").fadeOut();
+}
