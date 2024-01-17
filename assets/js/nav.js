dead1ee94eb2fa8abbbb0d3bde84f9f367d1d449
Rebecca White <rtw@null.net>
2024-01-17 02:21:24 +0000

    Updated Site
    The site is now updated. I may even stick with it.

--- assets/js/nav.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', (event) => {
-    let em = 10;
-    let sz = Math.max(3*em, 5.5*em-window.scrollY*0.2);
-    let opacity = 4 - window.scrollY / (5*em);
-    let nav = document.getElementsByClassName("nav-container")[0];
-    nav.style.opacity = opacity.toString();
-});
